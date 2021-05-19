d3dabe93dc054e475fc21b06815d3c4b8b4b502b
farah1979 <farahhisham1979@gmail.com>
2021-05-19 11:10:30 +0000

    Creat validate_data function to check if the values is correct

--- run.py
+++ run.py
@@ -20,6 +20,22 @@ def get_sales_data():
     print("Data should be sex numbers saperated by commas")
     print("Example: 10, 5, 8, 23, 12, 9\n")
     data_str = input("Enter your data here: ")
-    print(f"The data that provided is {data_str}")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+    
+
+def validate_data(values):
+    """
+    Check if the user enter sex integer values
+    """
+    try: 
+        if len(values) != 6:
+            raise ValueError(f"Exactly 6 values requierd, you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data {e}. Please try again.\n")
+    
+    
+
+
 
 get_sales_data()
